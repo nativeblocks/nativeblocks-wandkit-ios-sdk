d1ed180536ee5967b18b583eaa2968f44d1f686d
alireza <alireza@nativeblocks.io>
2024-12-01 23:13:40 +0300

    fix NativeblocksWandkit's dependencies

--- Package.swift
+++ Package.swift
@@ -9,9 +9,12 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .library(name: "NativeblocksWandkit", targets: ["Wandkit", "nativeblocks-ios-sdk", "AgoraRtcEngine_iOS"]),
+        .library(
+            name: "NativeblocksWandkit",
+            targets: ["NativeblocksWandkit"]
+        ),
     ],
-     dependencies: [
+    dependencies: [
         .package(url: "https://github.com/nativeblocks/nativeblocks-ios-sdk.git", from: "1.2.0"),
         .package(url: "https://github.com/AgoraIO/AgoraRtcEngine_iOS.git", from: "4.5.0"),
     ],
@@ -22,16 +25,14 @@ let package = Package(
             checksum: "1d02b42051ccfc38be8dc8db04e181d7bb7d86aefebacd8a83875c8bc541fa14"
         ),
         .target(
-            name: "nativeblocks-ios-sdk",
+            name: "NativeblocksWandkit",
             dependencies: [
-                .product(name: "nativeblocks-ios-sdk", package: "nativeblocks-ios-sdk")
-            ]
-        ),
-        .target(
-            name: "AgoraRtcEngine_iOS",
-            dependencies: [
-                .product(name: "AgoraRtcEngine_iOS", package: "AgoraRtcEngine_iOS")
-            ]
+                "Wandkit",
+                .product(name: "Nativeblocks", package: "nativeblocks-ios-sdk"),
+                .product(name: "RtcBasic", package: "AgoraRtcEngine_iOS", condition: .when(platforms: [.iOS])),
+                .product(name: "ReplayKit", package: "AgoraRtcEngine_iOS", condition: .when(platforms: [.iOS])),
+            ],
+            path: "./Sources/Empty"
         )
     ]
 )
