1b811c167da365ec61897d4ea454afaccf5719b8
alireza <alireza@nativeblocks.io>
2024-12-02 11:59:36 +0300

    update NativeblocksWandKit version to 1.0.0

--- NativeblocksWandkit.xcframework/ios-arm64_x86_64-simulator/NativeblocksWandkit.framework/Headers/NativeblocksWandkit.h
+++
@@ -1,9 +0,0 @@
-//
-//  NativeblocksWandkit.h
-//  NativeblocksWandkit
-//
-
-#import <Foundation/Foundation.h>
-
-FOUNDATION_EXPORT double NativeblocksWandkitVersionNumber;
-FOUNDATION_EXPORT const unsigned char NativeblocksWandkitVersionString[];
