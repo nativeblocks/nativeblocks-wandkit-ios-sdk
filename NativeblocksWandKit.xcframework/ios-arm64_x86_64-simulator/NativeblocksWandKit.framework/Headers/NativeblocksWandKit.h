3069acd3ff750bd4fb2939496e22298bd6f750a1
Ali Donyaie <ali@nativeblocks.io>
2025-05-07 14:01:25 +0330

    update wandkit boundle

---
+++ NativeblocksWandKit.xcframework/ios-arm64_x86_64-simulator/NativeblocksWandKit.framework/Headers/NativeblocksWandKit.h
@@ -0,0 +1,9 @@
+//
+//  NativeblocksWandKit.h
+//  NativeblocksWandKit
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double WandKitVersionNumber;
+FOUNDATION_EXPORT const unsigned char WandKitVersionString[];
