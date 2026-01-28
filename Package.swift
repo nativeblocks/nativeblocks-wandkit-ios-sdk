e8319e45e50eedc32275ce97c5a26e2256ea4514
alirezat775 <alireza@nativeblocks.io>
2026-01-28 11:51:25 +0400

    update version 1.2.0

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/nativeblocks/nativeblocks-ios-sdk", .upToNextMajor(from: "1.7.1")),
+        .package(url: "https://github.com/nativeblocks/nativeblocks-ios-sdk", .upToNextMajor(from: "1.8.0")),
     ],
     targets: [
         .binaryTarget(
             name: "NativeblocksWandkit",
-            url: "https://github.com/nativeblocks/nativeblocks-wandkit-ios-sdk/releases/download/1.1.1/NativeblocksWandkit.xcframework.zip",
-            checksum: "b2e9cf8a5d4c8a29e8c43cfd8f2947a4ec6092b53036d02c67a6a0e77d0d2dd9"
+            url: "https://github.com/nativeblocks/nativeblocks-wandkit-ios-sdk/releases/download/1.2.0/NativeblocksWandkit.xcframework.zip",
+            checksum: "8e5e584fb331998b7ac869a9ddd6124724a0004bf6e6029a8fa3ee4368be3bbb"
         ),
         .target(
             name: "NativeblocksWandkitPrivate",
